4e2abb2a1a0ead139d569990e61d2019038d33f5
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-02-02 18:57:20 +0100

    yeet

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import discord
 import asyncio
+import json
 
 f = open("token.txt")
 TOKEN = f.read().strip()
 f.close()
+f = open("error_codes.json")
+ERROR_CODES = json.load(f)
+f.close()
+
+LUUK_ID = "183315569745985545"
 
 client = discord.Client()
+
+def generate_error(code) :
+    return "ERROR {}: {}".format(code, ERROR_CODES[code])
 
 @client.event
 async def on_ready():
@@ -18,16 +27,20 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+    if message.content.startswith("ta!lmgtfy") :
+        args = message.content.split("ta!lmgtfy")[1].lstrip().rstrip().split(" ")
+        result = generate_error("101")
+        if len(args) > 0 and not (len(args) == 1 and args[0] == '') :
+            result = "<http://lmgtfy.com/?q="
+            for arg in args :
+                result += arg + "+"
+            result = result[:-1] + ">"
+        await client.send_message(message.channel, result)
+    if message.author.id == LUUK_ID :
+        if message.content.startswith("ta!reload_error_codes") :
+            f = open("error_codes.json")
+            ERROR_CODES = json.load(f)
+            f.close()
+            await client.send_message(message.channel, "No problem <:TsuAdorableBot:541315335169507345>")
 
 client.run(TOKEN)
